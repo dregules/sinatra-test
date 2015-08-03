2f3d88d493f0bfb6ef4fc683d936a3bb5589fbeb
Brandon Graves <bgraves14@gmail.com>
2015-08-03 12:17:40 +0100

    added cat

--- hi.rb
+++ hi.rb
@@ -1,15 +1,20 @@
 require 'sinatra'
 
 get '/' do
-  "Hello Brandon!"
+  "<div>
+  <img src='http://bit.ly/1eze8aE'>
+  </div>"
 end
 
 get '/secret' do
   "Hello secret Brandon!"
 end
 
+get '/diego' do
+  "diegorules!"
+end
+
 
 # get '/hi' do
 #   "Hello!"
 # end
-
